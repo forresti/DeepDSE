4c6ff13c1c9e19558c9c441386663cd40c996f67
Forrest Iandola <forresti@eecs.berkeley.edu>
2015-10-15 17:19:18 -0400

    sorting log filenames

--- parse_logs_NiN.py
+++ parse_logs_NiN.py
@@ -1,15 +1,15 @@
 from titan_runtime.parse_logs import get_latest_log
-from titan_runtime.parse_logs import get_current_accuracy_googlenet
+from titan_runtime.parse_logs import get_current_accuracy
 import titan_runtime.conf_rt as conf
 import os
 
 if __name__ == "__main__":
 
   jobs = os.listdir(conf.net_dir)
-  jobs = [j for j in jobs if 'googlenet' in j]
+  jobs = [j for j in jobs if 'NiN' in j]
   jobs = sorted(jobs)
   for logdir in jobs:
     logF = get_latest_log(conf.net_dir + '/' + logdir)
-    accuracy_dict = get_current_accuracy_googlenet(logF)
+    accuracy_dict = get_current_accuracy(logF)
     print '  net: %s, accuracy: %f, at iter %d' %(logdir, accuracy_dict['accuracy'], accuracy_dict['iter'])
 
