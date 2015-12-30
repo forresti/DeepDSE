19e7af6f129c663ab6d2d9d62654b6772956106f
Forrest Iandola <forresti@eecs.berkeley.edu>
2015-12-30 13:31:24 -0500

    minor tweaks to paths

--- titan_runtime/conf_rt.py
+++ titan_runtime/conf_rt.py
@@ -6,14 +6,19 @@
 
 #where are the nets to train?
 #net_dir = 'tests/fake_nets'
-net_dir = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_batchsize_sweep'
+#net_dir = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_batchsize_sweep'
+net_dir = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015'
+#net_dir = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015_debug_batchreduce'
 
 #titan user ID (for tracking the PBS scheduling)
 uname = 'forresti'
 #uname = 'worley'
 
-caffe_root = '/ccs/home/forresti/FireCaffe'
+caffe_root = '/ccs/home/forresti/FireCaffe_batchreduce'
 caffe_bin_computenode = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/bin'
 caffe_lib_computenode = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/lib'
-max_jobs = 1000
+#max_jobs = 1000
+max_jobs = 3
+
+walltime=2
 
--- titan_runtime/pbs_template.py
+++ titan_runtime/pbs_template.py
@@ -4,7 +4,12 @@ import conf_rt as conf
 # where path is relative to conf.net_dir
 def pbs_template(n_jobs, job_dicts):
     n_gpus = sum( [j['n_gpu'] for j in job_dicts] )
-    if n_gpus >= 125:
+
+    if hasattr(conf, 'walltime'):
+        walltime=conf.walltime
+    elif n_gpus >= 313:
+        walltime=12 #hours
+    elif n_gpus >= 125:
         walltime=6 #hours
     else:
         walltime=2 #hours
@@ -12,8 +17,8 @@ def pbs_template(n_jobs, job_dicts):
     st = """#!/bin/bash
 #PBS -A csc103
 #PBS -q batch
-#PBS -e ~/error.txt
-#PBS -o ~/output.txt
+#PBS -e /dev/null
+#PBS -o /dev/null
 #PBS -l gres=atlas1%atlas2"""
     st += '\n#PBS -l walltime=%d:00:00'%walltime
     st += "\n#PBS -l nodes=%d" %n_gpus #%n_jobs
@@ -30,6 +35,7 @@ def pbs_template(n_jobs, job_dicts):
     st += '\ncd $NETDIR'
     st += '\nnow=`date +%a_%Y_%m_%d__%H_%M_%S`'
     st += '\nexport MPICH_ALLREDUCE_NO_SMP=1'
+    st += '\nexport APRUN_BALANCED_INJECTION=80'
 
     n_jobs = min(n_jobs, len(job_dicts)) # making sure we're not going beyond our allocation
     for j in job_dicts:
