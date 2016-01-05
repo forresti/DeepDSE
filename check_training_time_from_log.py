d5d0710a1e2b240f85c5a7d5c5b50f7350f9e5e0
Forrest Iandola <forresti@eecs.berkeley.edu>
2016-01-05 17:48:47 -0500

    list of training timing logs

--- check_training_time_from_log.py
+++ check_training_time_from_log.py
@@ -1,19 +1,31 @@
 from titan_runtime.parse_logs import get_time_per_iter
-import titan_runtime.conf_rt as conf
+from titan_runtime.parse_logs import get_latest_log
+#import titan_runtime.conf_rt as conf
 import os
+
+
+#ASSUME: we have time_training_<datetime>.log in some of the net_dir subdirectories
 
 if __name__ == "__main__":
 
-  '''
-  #TODO: 100-iteration solver
-  cp 100 iteration solver to directory to evaluate
-  run 100-iterations solver (for various numbers of GPUs)
-  parse results
+  log_fnames = []
 
-  '''
+  #log_fnames.append('/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015_done/FireNet_8_fireLayers_base_64_64_64_incr_64_64_64_freq_2/train_Mon_2015_12_14__16_07_30.log')
+  #log_fnames.append('/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015_done/FireNet_8_fireLayers_base_r_64_64_incr_r_64_64_CEratio_1.000_freq_2/train_Sun_2015_12_20__19_35_20.log')
+  #log_fnames.append('/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015_done/FireNet_8_fireLayers_base_r_64_64_incr_r_64_64_CEratio_0.125_freq_2/train_Thu_2015_12_17__21_30_39.log')
 
-  #log_fname = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015_done/FireNet_8_fireLayers_base_64_64_64_incr_64_64_64_freq_2/train_Mon_2015_12_14__16_07_30.log'
-  log_fname = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015_done/FireNet_8_fireLayers_base_r_64_64_incr_r_64_64_CEratio_0.125_freq_2'
-  time_stats = get_time_per_iter(log_fname)
-  print time_stats
+  net_dir = '/lustre/atlas/scratch/forresti/csc103/dnn_exploration/nets_nov2015_done/'
+  for d in os.listdir(net_dir):
+    #TODO: possibly replace the following with 'get_latest_log'
+    train_dir = net_dir + '/' + d
+    for f in os.listdir(train_dir):
+      if 'time_training' in f:
+        log_fnames.append(train_dir + '/' + f)
+  print log_fnames
+
+  for log_fname in log_fnames:
+    print log_fname
+    time_stats = get_time_per_iter(log_fname)
+    print time_stats
+    print '' #newline
 
